9c1e1c65dbaf0fe49b6d79bb04e5956da82682ae
Hossam fahmi <hossameldinfahmi@gmail.com>
2023-03-29 23:47:37 +0200

    Add ESLint and Prettier for consistent code formatting

--- server/lib/index.ts
+++ server/lib/index.ts
@@ -1,6 +1,5 @@
 import { ErrorRequestHandler } from 'express';
-const asycnWrapper = (promise:Promise<void>)=> 
-    promise.then((data:any)=>[undefined,data])
-    .catch((err:any)=>[err]);
+const asycnWrapper = (promise: Promise<void>) =>
+  promise.then((data: any) => [undefined, data]).catch((err: any) => [err]);
 
 module.exports = asycnWrapper;
