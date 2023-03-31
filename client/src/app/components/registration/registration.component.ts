3ab35d3d768471150c630fd12231d6cf6b38ecc9
Hossam fahmi <hossameldinfahmi@gmail.com>
2023-03-31 06:33:43 +0200

    registration form validation and UI enhancements for registratio

--- client/src/app/components/registration/registration.component.ts
+++ client/src/app/components/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -8,13 +8,27 @@ import { ReactiveFormsModule } from '@angular/forms';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
-  registrationForm!: FormGroup; // declare the form group variable and initialize it to undefined
+
   message = "New here? Create a free account!";
   messageArray: string[] = [];
   typedMessage = "";
   submitted = false; // add a submitted property and set it to false
-  
+
   constructor(private formBuilder: FormBuilder) {}
+
+  registrationForm = new FormGroup ({
+    firstName: new FormControl(null, [Validators.required, Validators.minLength(3),Validators.maxLength(15)]),
+    lastName: new FormControl(null, [ Validators.required, Validators.minLength(3),Validators.maxLength(15)]),
+    email: new FormControl("user@gmail.com", [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    confirmPassword: new FormControl(null, [Validators.required, this.matchConfirmPassword.bind(this)])
+  }) 
+  matchConfirmPassword(control: AbstractControl): ValidationErrors | null {
+    const password = control.root.get('password');
+    return password && control.value !== password.value ? { 'passwordMismatch': true } : null;
+  }
+  
+  
 
   ngOnInit() {
     // Split the message into an array of characters
@@ -31,17 +45,6 @@ export class RegistrationComponent implements OnInit {
       }
     }, 100);
 
-    // initialize the form group and set validation rules
-    this.registrationForm = this.formBuilder.group({
-      firstName: ['', Validators.required],
-      lastName: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-      confirmPassword: ['', Validators.required],
-      fileUpload: [null, Validators.required]
-    }, {
-      validator: this.passwordMatchValidator // custom validator function to check if password and confirm password match
-    });
   }
 
   // custom validator function
@@ -52,8 +55,8 @@ export class RegistrationComponent implements OnInit {
   }
 
   // submit form function
-  onSubmit() {
+  submitRegisterForm(registrationForm:FormGroup) {
     this.submitted = true; // set submitted to true when the form is submitted
-    console.log(this.registrationForm.value);
+    console.log(registrationForm);
   }
 }
