93879b182f989099be1ebac9ae5569bc5679dbd5
Ali Ahmed Fathi <72993021+AliFathy-1999@users.noreply.github.com>
2023-04-03 06:17:14 +0200

    Add delete author function

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -15,42 +15,31 @@ export class LoginComponent {
   userName:String="";
   password:String="";
 constructor(private _AuthService :AuthService){
-  
+
 this.loginForm = new FormGroup({
   userName : new FormControl(null,[Validators.required,Validators.minLength(8)]),
   password : new FormControl(null,[Validators.required,Validators.minLength(8)])
-}) 
+})
 }
 
 
 login() {
-  
- 
+
+
     console.log(this.loginForm.value);
-    
+
     this._AuthService.login(this.loginForm.value).subscribe(
       (res) => {
         console.log(res.token);
         console.log(res);
-<<<<<<< HEAD
-        localStorage.setItem('token',res.token);
-=======
->>>>>>> a0f18f00090b5719da4c21bf63b0723e0c7575c0
       },
       (error: HttpErrorResponse) => {
         console.error('Error status code:', error.status);
         console.error('Error message:', error.message);
-<<<<<<< HEAD
+
+
       }
     );
-  
-=======
-        
-        
-      }
-    );
-  
-}
->>>>>>> a0f18f00090b5719da4c21bf63b0723e0c7575c0
+
 }
 }
