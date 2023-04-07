535ff98ad456a269afc049ce619fb4f4c77b0b7e
nadaezzat-99 <nadaezzat507@gmail.com>
2023-04-08 00:26:34 +0200

    Edit Book Retrieve

--- server/DB/models/book.ts
+++ server/DB/models/book.ts
@@ -2,6 +2,7 @@ import { Schema, model, ObjectId, Types } from 'mongoose';
 import { Book, BookModel, review } from '../schemaInterfaces';
 import mongoosePaginate from 'mongoose-paginate-v2';
 import { AppError, trimText } from '../../lib';
+import NextFunction from 'express';
 
 const schema = new Schema<Book>(
   {
@@ -89,7 +90,13 @@ schema.statics.getNewId = async () =>
 schema.virtual('totalRating').get(function () {
   if (!this.reviews?.length) return 0;
   const sum = this.reviews.reduce((acc, cur) => acc + cur.rating.valueOf(), 0);
+  // if (!this.ratingsNumber) this.averageRating = 0;
+  // this.averageRating = Math.floor(sum / this.ratingsNumber);
+  // console.log(this.averageRating);
+  // this.save();
   return sum;
+}).set(function () {
+  this.averageRating = Math.floor(this.totalRating / this.ratingsNumber);
 });
 
 schema.plugin(mongoosePaginate);
@@ -97,26 +104,28 @@ schema.plugin(mongoosePaginate);
 schema.statics.getBookById = async function (_id: Number) {
   return await Books.findById(_id)
     .populate({ path: 'authorId', select: ['firstName', 'lastName'] })
-    .populate({ path: 'categoryId', select: ['name'] })
+    .populate({ path: 'categoryId', select: "categoryId._id" })
     .populate({ path: 'reviews.user', select: ['firstName', 'lastName'] })
     .exec()
     .then((data) => data)
     .catch((err) => err);
 };
 
+
 schema.statics.editReviews = async function (data: { _id: Number; userId: ObjectId; comment: string; rating: number }) {
   let book = await Books.findOne({ _id: data._id });
+
   if (!book) {
-    throw new AppError (`No book with ID ${data._id}`, 400); 
+    throw new AppError(`No book with ID ${data._id}`, 400);
   }
 
-   let bookWithReview = await Books.findOneAndUpdate(
-      { _id: data._id, 'reviews.user': data.userId },
-      { $set: { 'reviews.$.comment': trimText(data.comment), 'reviews.$.rating': data.rating } },
-      { new: true }
-    );
+  let bookWithReview = await Books.findOneAndUpdate(
+    { _id: data._id, 'reviews.user': data.userId },
+    { $set: { 'reviews.$.comment': trimText(data.comment), 'reviews.$.rating': data.rating } },
+    { new: true }
+  );
 
-   if (!bookWithReview) {
+  if (!bookWithReview) {
     bookWithReview = await Books.findOneAndUpdate(
       { _id: data._id },
       {
@@ -130,19 +139,25 @@ schema.statics.editReviews = async function (data: { _id: Number; userId: Object
   return bookWithReview;
 };
 
-schema.pre('save', async function () {
+schema.pre('save', { document: true, query: true }, async function () {
   if (this.isNew) {
     this._id = await Books.getNewId();
   }
+  console.log('after update');
+  if (this.isModified('totalRating')) {
+    if (!this.ratingsNumber) this.averageRating = 0;
+    this.averageRating = Math.floor(this.totalRating / 5 / this.ratingsNumber);
+  } 
 });
 
-schema.pre('findOneAndUpdate', async function () {
-  console.log('hi');
-  const doc = await this.model.findOne(this.getQuery());
-  if (!doc.ratingsNumber) doc.averageRating = 0;
-  else doc.averageRating = Math.floor((doc.totalRating/5 / doc.ratingsNumber));
-  doc.save();
-});
+// schema.pre('findOneAndUpdate', async function () {
+//   console.log('hi');
+//   const doc = await this.model.findOne(this.getQuery());
+//   console.log(doc);
+//   if (!doc.ratingsNumber) doc.averageRating = 0;
+//   else doc.averageRating = Math.floor((doc.totalRating/5 / doc.ratingsNumber));
+//   doc.save();
+// }); 
 
 const Books = model<Book, BookModel>('Books', schema);
 
