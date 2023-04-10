2abe7ff744b0f469740d2dc365ef7e5834d9f0b1
Hossam fahmi <hossameldinfahmi@gmail.com>
2023-04-10 23:57:12 +0200

    Restruct The User Books workfolw

--- server/controllers/userBooks.ts
+++ server/controllers/userBooks.ts
@@ -1,39 +1,25 @@
-import { User, UserBooks } from '../DB/schemaInterfaces';
+import { UserBooks } from '../DB/models/userBooks';
+// import { User, UserBooksType } from '../DB/schemaInterfaces';
 const Users = require('../DB/models/user');
 import { AppError, asycnWrapper } from '../lib/index';
 import { Types } from 'mongoose';
 
-// Get All user Book's
-const getUserBooks = async (userId: string) => {
-  console.log();
-
-  const user = await getUserById(userId);
-  await populateUserBooks(user);
-
-  const books = mapUserBooks(user);
-
-  const userData = {
-    id: user._id,
-    firstName: user.firstName,
-    lastName: user.lastName,
-    userName: user.userName,
-    image: user.pImage,
-    books: books,
-  };
-  return userData;
-};
-
 const getUserById = async (userId: String) => {
   const userPromise = Users.findById(userId);
+  console.log('userPromise:', userPromise); // Add this line
   const [userErr, user] = await asycnWrapper(userPromise);
+  console.log('userErr:', userErr); // Add this line
+  console.log('user:', user); // Add this line
   if (userErr || !user) {
     throw new AppError('User not found', 404);
   }
   return user;
 };
 
-const populateUserBooks = async (user: UserBooks) => {
-  const populateBooks = user.populate({
+const getUserBooks = async (userId: string) => {
+  console.log('userId:', userId); // Add this line
+
+  const userBooks = await UserBooks.findOne({ userId }).populate({
     path: 'books.book',
     select: 'name bookImage authorId averageRating ratingsNumber',
     populate: {
@@ -42,52 +28,30 @@ const populateUserBooks = async (user: UserBooks) => {
     },
   });
 
-  const [populateErr] = await asycnWrapper(populateBooks);
-  if (populateErr) {
-    throw new AppError('Error while populating books', 500);
+  if (!userBooks) {
+    throw new AppError('User not found', 404);
   }
-};
 
-const mapUserBooks = (user: any) => {
-  return user.books.map((book: any) => {
+  const books = userBooks.books.map((book: any) => {
     return {
       bookId: book.book._id,
       name: book.book.name,
       cover: book.book.bookImage,
       author: `${book.book.authorId.firstName} ${book.book.authorId.lastName}`,
-      rating: book.book.ratingsNumber,
+      rating: book.rating,
       averageRating: book.book.averageRating,
     };
   });
-};
 
-// Update rate and status
-const updateBook = async (userId: string, bookId: string, newRate: number, newShelf: string) => {
-  const user = await getUserById(userId);
-
-  const book = await user.books.find((book: any) => book.book.toString() === bookId.toString());
-  console.log(book);
-
-  if (!book) {
-    throw new Error("Book not found in user's list");
-  }
-
-  book.rate = newRate;
-
-  book.shelve = newShelf;
-  await user.save();
-
-  return {
-    bookId: book.book._id,
-    name: book.book.name,
-    cover: book.book.bookImage,
-    author: `${book.book.authorId.firstName} ${book.book.authorId.lastName}`,
-    rating: book.book.ratingsNumber,
-    averageRating: book.book.averageRating,
+  const userData = {
+    id: userBooks.userId,
+    books: books,
   };
+
+  return userData;
 };
 
 module.exports = {
   getUserBooks,
-  updateBook,
+  // updateBook,
 };
