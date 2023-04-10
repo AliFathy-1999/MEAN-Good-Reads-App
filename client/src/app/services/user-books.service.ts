5dbd192e4c27edbc9bfd0f98151ca79ed4454cce
MennaRaafat <menna.raafat.hamed@commerce.helwan.edu.eg>
2023-04-10 06:15:57 +0200

    Validations on book form in admin and login page

--- client/src/app/services/user-books.service.ts
+++ client/src/app/services/user-books.service.ts
@@ -2,13 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/Observable';
 import { Book } from '../dataTypes/typesModule';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserBooksService {
 
-  constructor(private _http:HttpClient) { }
+  constructor(private _http:HttpClient,private cookieService: CookieService) { }
+
+  isLogged(): boolean {
+    return this.cookieService.get('token') != null;
+  }
 
   getAllBooks(page:number,limit:number): Observable<Book>{
    return this._http.get<Book>(`http://localhost:3000/books?page=${page}&limit=${limit}`);
